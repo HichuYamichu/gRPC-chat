0de2f30cb6d0f46527b4de11dfee4686683d014d
HichuYamichu <hichuyamichu@gmail.com>
2019-08-05 18:44:26 +0200

    added simple GUI

--- client/main.go
+++ client/main.go
@@ -6,52 +6,123 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"os"
 	"strings"
 
 	"github.com/HichuYamichu/gRPC-chat/proto"
+	"github.com/jroimartin/gocui"
 	"google.golang.org/grpc"
 )
 
-func readStream(stream proto.Chat_ChatClient) {
+var addr = flag.String("addr", "localhost:3000", "gRPC server address")
+var name = flag.String("name", "jan", "username")
+var stream proto.Chat_ChatClient
+
+func layout(g *gocui.Gui) error {
+	maxX, maxY := g.Size()
+	typerY := maxY / 8
+	chatY := maxY - (typerY) - 1
+
+	if v, err := g.SetView("typer", 0, chatY, maxX-1, maxY-1); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		v.Wrap = true
+		v.Editable = true
+
+		if _, err := g.SetCurrentView("typer"); err != nil {
+			return err
+		}
+	}
+
+	if v, err := g.SetView("chatBox", 0, 0, maxX-1, chatY-1); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		v.Autoscroll = true
+		v.Wrap = true
+	}
+
+	return nil
+}
+
+func quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
+}
+
+func send(g *gocui.Gui, v *gocui.View) error {
+	typer, err := g.View("typer")
+	if err != nil {
+		return err
+	}
+	reader := bufio.NewReader(typer)
+	text, _ := reader.ReadString('\n')
+	text = strings.Replace(text, "\n", "", 1)
+	text = strings.Replace(text, "\r", "", 1)
+	msg := &proto.Message{Author: *name, Content: text}
+	stream.Send(msg)
+	if err != nil {
+		return err
+	}
+	typer.Clear()
+	typer.SetCursor(0, 0)
+	return nil
+}
+
+func readStream(stream proto.Chat_ChatClient, g *gocui.Gui) {
 	for {
 		msg, err := stream.Recv()
 		if err != nil {
 			log.Fatal(err)
 		}
-		m := fmt.Sprintf("<%v> %v", msg.Author, msg.Content)
-		_, err = fmt.Fprintln(os.Stdout, m)
-		if err != nil {
-			log.Fatal(err)
-		}
-
+		g.Update(func(g *gocui.Gui) error {
+			chatBox, err := g.View("chatBox")
+			if err != nil {
+				return err
+			}
+			m := fmt.Sprintf("<%v> %v", msg.Author, msg.Content)
+			_, err = fmt.Fprintln(chatBox, m)
+			if err != nil {
+				return err
+			}
+			return nil
+		})
 	}
 }
 
-var addr = flag.String("addr", "localhost:3000", "gRPC server address")
-var name = flag.String("name", "jan", "username")
-
 func main() {
 	flag.Parse()
+
 	conn, err := grpc.Dial(*addr, grpc.WithInsecure())
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer conn.Close()
 	c := proto.NewChatClient(conn)
-	stream, err := c.Chat(context.Background())
+	stream, err = c.Chat(context.Background())
 	if err != nil {
 		return
 	}
-
 	stream.Send(&proto.Message{Author: *name, Content: ""})
 
-	go readStream(stream)
-	for {
-		reader := bufio.NewReader(os.Stdin)
-		text, _ := reader.ReadString('\n')
-		text = strings.Replace(text, "\n", "", 1)
-		msg := &proto.Message{Author: *name, Content: text}
-		stream.Send(msg)
+	g, err := gocui.NewGui(gocui.OutputNormal)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer g.Close()
+	g.Highlight = true
+	g.Cursor = true
+	g.SelFgColor = gocui.ColorRed
+	g.SetManagerFunc(layout)
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		log.Fatal(err)
+	}
+	if err := g.SetKeybinding("", gocui.KeyEnter, gocui.ModNone, send); err != nil {
+		log.Fatal(err)
+	}
+
+	go readStream(stream, g)
+
+	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+		log.Fatal(err)
 	}
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,9 @@ go 1.12
 
 require (
 	github.com/golang/protobuf v1.3.2
+	github.com/jroimartin/gocui v0.4.0
+	github.com/mattn/go-runewidth v0.0.4 // indirect
+	github.com/nsf/termbox-go v0.0.0-20190624072549-eeb6cd0a1762 // indirect
 	golang.org/x/net v0.0.0-20190311183353-d8887717615a
 	google.golang.org/grpc v1.22.0
 )
